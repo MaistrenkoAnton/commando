c7c4b897cca835a51946b859dd023aecebba8596
uCodista <kapinus.artem@gmail.com>
2016-02-22 10:35:52 +0200

    'Client' folder deleted.

--- server/src/stores/urls.py
+++ server/src/stores/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^add-item/$', ItemAddView.as_view(), name='add_item'),
     url(r'^update-delete-item/(?P<pk>\d+)/$', ItemUpdateView.as_view(), name='update_delete_item'),
     url(r'^account-store/(?P<pk>\d+)/$', views.StoreDetailView.as_view(), name='account_store'),
+    url(r'^itemlist/(?P<category_pk>\d+)/(?P<store_pk>\d+)/$', views.StoreItemsListView.as_view(), name='item_list'),
     url(r'^$', views.StoreListView.as_view(), name='store_list'),
 ]
--- server/src/stores/views.py
+++ server/src/stores/views.py
@@ -1,4 +1,9 @@
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from haystack.query import SearchQuerySet
+from catalogue.haystack_serializers import ItemListHaystackSerializer
+from catalogue.models import Item
 from .models import Store
 from .serializers import StoreListSerializer
 
@@ -19,3 +24,20 @@ class StoreDetailView(generics.RetrieveAPIView):
     serializer_class = StoreListSerializer
     model = Store
     queryset = Store.objects.all()
+
+
+class StoreItemsListView(APIView):
+    """
+    List Items
+    category_pk -- filter by category
+    store_pk -- filter by store
+    """
+    serializer = ItemListHaystackSerializer
+    model = Item
+
+    def get(self, request, category_pk, store_pk):
+        response_data = self.serializer(self._get_queryset(category_pk, store_pk), many=True).data
+        return Response(response_data)
+
+    def _get_queryset(self, category_pk, store_pk):
+        return SearchQuerySet().models(self.model).filter(category=category_pk).filter(store=store_pk)
